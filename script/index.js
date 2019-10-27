74b0f548836798189c330e9fd0c90ad7069a063a
Harry Senjaya <harxsenjaya56@gmail.com>
2019-10-27 22:20:28 +0700

    nav final

--- script/index.js
+++ script/index.js
@@ -1,12 +1,23 @@
-function myFunction() {
+function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function(e) {
+window.onclick = function (e) {
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+    var myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
   }
+  // else if (!e.target.matches('.kategori')) {
+  //   var myDropdown = document.getElementById("kategori");
+  //   if (myDropdown.classList.contains('show')) {
+  //     myDropdown.classList.remove('show');
+  //   }
+  // }
 }
+
+// function dropdownKategori() {
+//   document.getElementById("kategori").classList.toggle("show");
+// }
+

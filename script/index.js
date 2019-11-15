512cc9c5b3bebdc5dbdf60a456a969abcb29cfa7
Harry Senjaya <harxsenjaya56@gmail.com>
2019-11-15 16:09:52 +0700

    kategori fix

--- script/index.js
+++ script/index.js
@@ -2,39 +2,45 @@ function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// window.onclick = function (e) {
-//   if (!e.target.matches('.dropbtn')) {
-//     var myDropdown = document.getElementById("myDropdown");
-//     if (myDropdown.classList.contains('show')) {
-//       myDropdown.classList.remove('show');
-//     }
-//   }
-//   // else if (!e.target.matches('.kategori')) {
-//   //   var myDropdown = document.getElementById("kategori");
-//   //   if (myDropdown.classList.contains('show')) {
-//   //     myDropdown.classList.remove('show');
-//   //   }
-//   // }
-// }
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+  // else if (!e.target.matches('.kategori')) {
+  //   var myDropdown = document.getElementById("kategori");
+  //   if (myDropdown.classList.contains('show')) {
+  //     myDropdown.classList.remove('show');
+  //   }
+  // }
+}
 
 function dropdownKategori() {
   var x = document.getElementById('kategori');
-	
-	if(x.className.indexOf("w3-show") == -1){
-		x.className += " w3-show";
-	}
-	else{
-		x.className = x.className.replace(" w3-show","");
-	}}
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+    var kategori = document.getElementById('dropKategori');
+    kategori.innerHTML = "Kategori <i class='fa fa-caret-down'></i>"
+  }
+  else {
+    x.className = x.className.replace(" w3-show", "");
+    var kategori = document.getElementById('dropKategori');
+    kategori.innerHTML = "Kategori <i class='fa fa-caret-right'></i>"
+  }
 
-function login(){
+
+}
+
+function login() {
   location.replace("SignInPage.html");
 }
 
-function register(){
+function register() {
   location.replace("SignUpPage.html");
 }
 
-function home(){
+function home() {
   location.replace("index.html");
 }

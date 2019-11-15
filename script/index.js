83252efbfff0a7e00f52300d37507aa029a6ee09
Harry Senjaya <harxsenjaya56@gmail.com>
2019-11-15 15:57:43 +0700

    kategori

--- script/index.js
+++ script/index.js
@@ -2,20 +2,20 @@ function dropdown() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.onclick = function (e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-  // else if (!e.target.matches('.kategori')) {
-  //   var myDropdown = document.getElementById("kategori");
-  //   if (myDropdown.classList.contains('show')) {
-  //     myDropdown.classList.remove('show');
-  //   }
-  // }
-}
+// window.onclick = function (e) {
+//   if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("myDropdown");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+//   // else if (!e.target.matches('.kategori')) {
+//   //   var myDropdown = document.getElementById("kategori");
+//   //   if (myDropdown.classList.contains('show')) {
+//   //     myDropdown.classList.remove('show');
+//   //   }
+//   // }
+// }
 
 function dropdownKategori() {
   var x = document.getElementById('kategori');

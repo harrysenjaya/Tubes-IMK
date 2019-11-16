e8700cb107905211ebc05d23a3220f06ec6e8b5c
Harry Senjaya <harxsenjaya56@gmail.com>
2019-11-17 00:23:13 +0700

    auto complete smua page

--- script/index.js
+++ script/index.js
@@ -44,3 +44,88 @@ function home() {
   location.replace("index.html");
 }
 
+function autocomplete(input, arr) {
+  var currentFocus;
+
+  input.addEventListener("input", function (e) {
+      var container, matchelem, i, val = this.value;
+      closeAllLists();
+      if (!val) {
+          return false;
+      }
+      currentFocus = -1;
+      container = document.createElement("DIV");
+      container.setAttribute("id", this.id + "autocomplete-list");
+      container.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(container);
+      for (i = 0; i < arr.length; i++) {
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+              matchelem = document.createElement("DIV");
+              matchelem.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+              matchelem.innerHTML += arr[i].substr(val.length);
+              matchelem.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+              matchelem.addEventListener("click", function (e) {
+                  input.value = this.getElementsByTagName("input")[0].value;
+                  closeAllLists();
+              });
+              container.appendChild(matchelem);
+          }
+      }
+  });
+
+  input.addEventListener("keydown", function (e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) {
+          x = x.getElementsByTagName("div");
+      }
+      if (e.keyCode == 40) {
+          currentFocus++;
+          addActive(x);
+      }
+      else if (e.keyCode == 38) {
+          currentFocus--;
+          addActive(x);
+      }
+      else if (e.keyCode == 13) {
+          e.preventDefault();
+          if (currentFocus > -1) {
+              if (x) {
+                  x[currentFocus].click();
+              }
+          }
+      }
+  });
+
+  function addActive(x) {
+      if (!x) {
+          return false;
+      }
+      removeActive(x);
+      if (currentFocus >= x.length) {
+          currentFocus = 0;
+      }
+      if (currentFocus < 0) {
+          currentFocus = (x.length - 1);
+      }
+      x[currentFocus].classListAdd("autocomplete-active");
+  }
+
+  function removeActive(x) {
+      for (var i = 0; i < x.length; i++) {
+          x[i].classList.remove("autocomplete-active");
+      }
+  }
+
+  function closeAllLists(elmnt) {
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+          if (elmnt != x[i] && elmnt != input) {
+              x[i].parentNode.removeChild(x[i]);
+          }
+      }
+  }
+
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
